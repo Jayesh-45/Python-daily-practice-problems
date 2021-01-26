129902fb9f1b89a21892b418867533ebe33f36a1
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2021-01-26 21:35:49 +0530

    Update Solutions_day_6.py

--- Day6/Solutions_day_6.py
+++ Day6/Solutions_day_6.py
@@ -40,3 +40,44 @@ print("The Final Dictionary after concatinating the above three dictionaries is:
 
 ____
 
+#2. Write a program to check if a given key exists in a Dictionary
+
+Dict2={}
+key=''
+value=''
+n=int(input("Enter the number of Keys you want in your Dictionary:"))
+i=1
+
+while(i<=n):
+    key =input()
+    value=input()
+    Dict2[key] = value
+    i=i+1
+
+print("Your final dictionary is:",Dict2)
+
+check=input("Enter the key you want to check in your dictionary:")
+if check in Dict2:
+    print("Yes,",check,"is present in",Dict2)
+else:
+    print("No,",check,"is not present in",Dict2)
+
+____
+
+#3. Write a program to generate and print a dictionary that contains a no. between 1 and n
+# In the form of (x,x*x)
+# Sample n=5
+# Output: 1:1,2:4,3:9,4:16,5:25
+
+dict={}
+n=int(input("Enter the value of N:"))
+data=0
+score=0
+i=1
+
+while(i<=n):
+    data =i
+    score =i*i
+    dict[data] = score
+    i=i+1
+print("The Final Dictionary is:",dict)
