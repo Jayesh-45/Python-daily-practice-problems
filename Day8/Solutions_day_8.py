bd3895c349e5bbe21b901bcf35a62d16e2f88716
Jayesh Bangar <65165798+Jayesh-45@users.noreply.github.com>
2021-01-30 21:37:05 +0530

    Update Solutions_day_8.py

--- Day8/Solutions_day_8.py
+++ Day8/Solutions_day_8.py
@@ -1,3 +1,34 @@
+# 1. Write a program to create a dictionary and access all elements using key and value.
+#Dictionary is unordered changeable and duplicates are not allowed
+
+puppy = {"Mutable":"It can be changed",
+         "Immutable":"It cannot be changed",
+         "Wise":"The person who is morally correct",
+         "Manmade":"The objects that were not readily available in nature."}
+print("Search for the meanings of the following words:\n",
+      "1. Immutable\n","2. Mutable\n","3. Wise\n","4. Manmade\n")
+
+word = input()
+print(puppy[word])
+print("The dictionary is")
+
+count=1
+for i in puppy:
+    print(count,i,puppy[i])
+    count=count+1
+
+____
+
+#2. Write a program to add a key to a dictionary.
+
+
+s={1:"Yes",2:"This",3:"Is a"}
+print("Current dictionary is",s)
+key = int(input("Enter the key"))
+value = input("Enter the value")
+s[key] = value
+print("The final dict is",s)
+
 ____
 
 #3. Write a program to print all the unique values of a Dictionary.
